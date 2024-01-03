9cc6413d55b2e1ef0d1c5ffb9895f6933d170c0e
Greiskel <greiskel.sanchez@gmail.com>
2024-01-03 20:44:45 +0100

    modificando items

--- react_aframe/src/Components/Home.jsx
+++ react_aframe/src/Components/Home.jsx
@@ -8,26 +8,26 @@ const Home = () => {
   return (
     <div>
       <a-scene>
-        <a-sphere id="home" position="-5 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="-9 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="-1 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="3 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="7 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="11 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="15 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
-        <a-sphere id="home" position="-13 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="./img/items/Yellow Box.glb"></a-sphere>
+        <a-sphere id="home" position="-5 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="-9 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="-1 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="3 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="7 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="11 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="15 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
+        <a-sphere id="home" position="-13 5 -9.8394" scale="1 1 1" radius="1.25" color="white" animation="property: rotation; to: 0 360 0; loop: true; dur: 5000" gltf-model="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/YellowBox.glb?v=1704310607568"></a-sphere>
 
         <a-sky src="./img/Items/holographic_landscape.jpg"></a-sky>
 
         <a-assets id="scene">
-          <img id='logo' src="./img/items/symfony_black_03.png" alt="symfony" />
-          <img id='logo0' src="./img/items/GitHub-logo.png" alt="github" />
-          <img id='logo1' src="./img/items/php_logo.png" alt="" />
-          <img id='logo2' src="./img/items/JavaScript-Symbol.png" alt="javascript" />
-          <img id='logo3' src="./img/items/wordpress.png" alt="wordpress" />
-          <img id='logo4' src="./img/items/html.png" alt="html" />
-          <img id='logo5' src="./img/items/csslogo.png" alt="css" />
-          <img id='logo6' src="./img/items/MySQL-logo.png" alt="mysql" />
+          <img id='logo' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/symfonyblack03.png?v=1704310806245" alt="symfony" />
+          <img id='logo0' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/GitHublogo.png?v=1704310971902" alt="github" />
+          <img id='logo1' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/phplogo.png?v=1704310811665" alt="php" />
+          <img id='logo2' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/JavaScriptSymbol.png?v=1704310826156" alt="javascript" />
+          <img id='logo3' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/wordpress.png?v=1704310796716" alt="wordpress" />
+          <img id='logo4' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/html.png?v=1704310936147" alt="html" />
+          <img id='logo5' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/csslogo.png?v=1704310932031" alt="css" />
+          <img id='logo6' src="https://cdn.glitch.global/e143a3cd-7d15-4fb3-b52f-fc4c69a22d2f/MySQLlogo.png?v=1704310818439" alt="mysql" />
         </a-assets>
 
         <a-image src="#logo"
